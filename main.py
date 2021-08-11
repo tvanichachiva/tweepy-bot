5c835b51f753d2381eff0a4f5f1c10cd1320704b
tvani <tvanichachiva@gmail.com>
2021-08-10 19:11:52 -0500

    Added additional replies to a list and have the reply function randomly choose a reply so that the replies are varied

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import tweepy
 import pandas as pd
 import datetime
+import random
 
-#auth
+# auth
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 A_TOKEN = ''
 A_SECRET = ''
 
-#Setup API access
+# Setup API access
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(A_TOKEN, A_SECRET)
 
 api = tweepy.API(auth)
 
-#Check connection
+# Check connection
 api.verify_credentials()
 
 
-# Create function to extract tweet object attributes from a given time framge and then create a dataframe to store tweets
+# Create function to extract tweet object attributes from a given time frame
+# Then create a dataframe to store tweets
 def get_tweets(tweet_object):
     # creating an empty list for tweet objects
     tweets = []
@@ -26,7 +28,6 @@ def get_tweets(tweet_object):
     # Determine start of week and end of week
     today = datetime.date.today()
     start_of_week = pd.to_datetime(today - datetime.timedelta(days=today.weekday()))
-    end_of_week = pd.to_datetime(start_of_week + datetime.timedelta(days=6))
 
     # loop through tweet objects to extract pertinent attributes
     for t in tweet_object:
@@ -63,54 +64,68 @@ def get_tweets(tweet_object):
     # convert UTC to Central time
     df['created_local'] = df['created'].dt.tz_localize('UTC').dt.tz_convert('US/Central')
 
-    # sort by created_local
-    # df = df.sort_values(['created_local'], ascending=True)
-
     return df
 
-#Get most popular tweet
+
+# Get most popular tweet
 def get_popular(df):
-    #Get most popular tweet
+    # Get most popular tweet
     max = df['popularity'].max()
     id = df[df['popularity'] == max]['tweet_id'].values[0]
     rt = df[df['popularity'] == max]['rts'].values[0]
     fav = df[df['popularity'] == max]['favs'].values[0]
-    return [id,rt,fav]
+    return [id, rt, fav]
+
 
 # Retweet the most popular tweet of the week
 def rt_popular(df):
-    # Pass most popular tweet into the
+    # Get tweet ID of most popular tweet
     id = get_popular(df)[0]
 
     # Retweet most popular tweet
     api.retweet(id)
 
+
 # Favorite the most popular tweet of the week
 def fav_popular(df):
-    # Pass most popular tweet into the
+    # Get tweet ID of most popular tweet
     id = get_popular(df)[0]
 
     # Retweet most popular tweet
     api.create_favorite(id)
 
+
 # Reply to the most popular tweet of the week
 def reply_popular(df):
-    # Pass most popular tweet into the
+    # Get tweet ID of most popular tweet
     id = get_popular(df)[0]
 
+    # Static list of responses that will be chosen by random
+    reply = ['YAASSS! This tweet was a banger! We have no choice but to #STAN #SoSofia',
+             'This tweet is #SoSofia',
+             'Imma STANfia #SoSofia',
+             'Killing the Twitter game! #SoSofia',
+             'This tweet was #GIRLBOSS #SoSofia',
+             'Girlie keep it up! WE STAN! #SoSofia',
+             'Your tweets are golden we must protect them AT ALL COST! #STAN #SoSofia']
+
+    # Generate a random number that will be used to index into the reply list
+    num = random.randint(0, 6)
+
     # Reply most popular tweet
-    api.update_status(f'@wisdomberryson YAASSS! This tweet was a banger! We have no choice but to #STAN', id)
+    api.update_status(f'@wisdomberryson {reply[num]}', id)
 
-#Get Sofia's tweets
+
+# Get Sofia's tweets
 user = 'wisdomberryson'
-sofia = api.user_timeline(user, count = 500)
-sofia_tweets= get_tweets(sofia)
+sofia = api.user_timeline(user, count=500)
+sofia_tweets = get_tweets(sofia)
 
-#Retweet most popular tweet
+# Retweet most popular tweet
 rt_popular(sofia_tweets)
 
-#Favorite most popular tweet
+# Favorite most popular tweet
 fav_popular(sofia_tweets)
 
-#Reply to Sofia's most popular tweet
+# Reply to Sofia's most popular tweet
 reply_popular(sofia_tweets)
